0fcb1a0dbc2d35db68d21d947ce2e76f30025004
karla aqueveque <aquevequekarla@gmail.com>
2023-07-25 15:50:22 -0400

    git add

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,32 +1,45 @@
 const analyzer = {
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    //se crea un const y le di parametros. Words es igual a texto sin espacios en blanco, y dividido en palabras.
+    // trim = elimina espacios en blanco split = separación de la cadena Lenght= Para saber la cantidad de elementos de un array 
+    // con la expresion regular le damos parametros a cada metodo
     const words = text.trim().split(/\s+/);
     return words.length;
   },
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    // Se utiliza solo return 
     return text.length;
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    // La funcion me devolvera la cadena de texto, pero le indico que que todos los espacios en blanco, los puntos y comas, 
+    //los reemplace por nada, 
     return text.replaceAll(" ", "").replaceAll(".", "").replaceAll(",", "").length;
   },
   getAverageWordLength: (text) => {
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    //const contador me guardara el resultado por eso lo dejamos en 0
+    // se crea un const y la funcion con los parametros indicados con la expresion regular
+    //Se recorre el largo del string con un bucle for, y en cada iteracion se suma el largo de cada palabra
     let contador = 0;
     const palabras = text.replace("/[ ]+/g", " ").trim().split(" ");
     for (let i = 0; i < palabras.length; i++) {
       contador = contador + palabras[i].length;
     }
     return Number((contador / palabras.length).toFixed(2));
+    //retorna contador dividido por el largo total de las palabras. Con tofixed redondeamos los decimales
   },
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    //const texto, seperar el texto por espacios, (' ') en blanco
+    // let contador y un bucle for para sumar solo los number, el ciclo lo dara en inicio hasta la cantidad del string
+    // por cada iteracion le suma 1 solo si es numero
     const textSplit = text.split(' ')
     let contador = 0;
     for (let i = 0; i < textSplit.length; i++) {
-      if (Number(textSplit[i])) {
+      if (Number(textSplit[i])) { //si textsplit es un numero suma si no lo salta
         contador++;
       }
     }
@@ -38,7 +51,7 @@ const analyzer = {
     let contador = 0;
     for (let i = 0; i < textSplit.length; i++) {
       if (Number(textSplit[i])) {
-        contador = contador + Number(textSplit[i])
+        contador = contador + Number(textSplit[i]) // se suma el valor del numero
       }
     }
     return contador;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
 
-
+//creo las variable con const comenzando por textarea llamada cajatexto, luego una por cada li. 
+//Con el metodo querySelector llamo a los selectores del documento html que queremos "enlazar"
+//usando los atributos dados en el readme
 const cajaTexto = document.querySelector("textarea[name=user-input]");
 const liUno = document.querySelector('[data-testid="word-count"]');
 const liDos = document.querySelector('[data-testid="character-count"]');
@@ -10,6 +12,11 @@ const liTres = document.querySelector('[data-testid="character-no-spaces-count"]
 const liCuatro = document.querySelector('[data-testid="word-length-average"]');
 const liCinco = document.querySelector('[data-testid="number-count"]');
 const liSeis = document.querySelector('[data-testid="number-sum"]');
+
+
+//Aca utilizo el metodo addEventListener con un keyup para detectar cuando hay una accion que presiona la tecla
+//Luego de que ocurre el evento, creo una con innerHTML 
+
 
 cajaTexto.addEventListener("keyup", function () {
   liUno.innerHTML = 'Palabras: ' + analyzer.getWordCount(cajaTexto.value);
@@ -20,10 +27,14 @@ cajaTexto.addEventListener("keyup", function () {
   liSeis.innerHTML = 'Suma Números: ' + analyzer.getNumberSum(cajaTexto.value);
 });
 
+//Creo un const para el boton "limpiar campo"  con el metodo getElementById con el id del boton
+//Nuevamente con el metodo addEventListener buscamos detectar evento "click"
+//Indicamos que textaarea debe quedar "vacio" limpio" cada vez que se presione el boton 
+
 const botonReset = document.getElementById("reset-button");
 botonReset.addEventListener("click", function () {
   botonReset.addEventListener("mouseover", () => {
-
+    // evento mouseover Cuando el puntero del mouse se mueve sobre un elemento 
   });
   cajaTexto.value = "";
 });
